75d1bec531335a08f8c2b6c466f70e98c95f95a4
Sangdae <mousesd@gmail.com>
2022-10-03 09:37:36 +0900

    1.Building a web app with Spring Boot

---
+++ chapter01/src/main/java/net/homenet/chapter01/controller/HomeController.java
@@ -0,0 +1,15 @@
+package net.homenet.chapter01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import reactor.core.publisher.Mono;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public Mono<String> home() {
+        return Mono.just("home");
+    }
+
+}
